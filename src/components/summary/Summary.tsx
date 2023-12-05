57cd68194bdfb88e2df6d4e4a7ed3a6d33d74fa6
Facundo Grasso <facundofggrasso@gmail.com>
2023-12-05 14:57:39 -0300

    Add Summary to UI

--- src/components/summary/Summary.tsx
+++ src/components/summary/Summary.tsx
@@ -1,10 +1,46 @@
+//#region Dependency list
 import { FunctionComponent } from "react";
-//import styles from "./Summary.module.scss";
+import { shiftState } from "../../types/job/Shift";
+import styles from "./Summary.module.scss";
+import { jobPosition } from "../../types/job/Position";
+import { getStringDMY } from "../../services/dateService";
+//#endregion
 
-type thisProps = unknown;
+type thisProps = {
+    shiftList: shiftState[];
+    position: jobPosition;
+};
 
-const Summary: FunctionComponent<thisProps> = () => {
-    return <></>;
+const Summary: FunctionComponent<thisProps> = ({ shiftList, position }) => {
+    function getStartDate(): string {
+        if (!shiftList || shiftList.length <= 0) return "";
+
+        const firstDate = shiftList[0].date;
+        return getStringDMY(firstDate);
+    }
+
+    function getEndDate(): string {
+        if (!shiftList || shiftList.length <= 0) return "";
+
+        const firstDate = shiftList[shiftList.length - 1].date;
+        return getStringDMY(firstDate);
+    }
+
+    function getTotal(): number {
+        let total = 0;
+        shiftList.forEach(
+            (shift) => (total += shift.hoursWorked * position.hourPrice)
+        );
+        return total;
+    }
+
+    return (
+        <div className={styles.summaryBody}>
+            {getStartDate() && getEndDate() && (
+                <p>{`Total made from ${getStartDate()} to ${getEndDate()}: ${getTotal()}`}</p>
+            )}
+        </div>
+    );
 };
 
 export default Summary;
